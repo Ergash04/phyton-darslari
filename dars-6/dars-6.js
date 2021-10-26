81beb3c626344adea24395c8016b256dc8a193c5
Ergash04 <93190932+Ergash04@users.noreply.github.com>
2021-10-26 15:48:21 +0500

    frond end darslari

---
+++ dars-6/dars-6.js
@@ -0,0 +1,38 @@
+// var name = 'block-1';
+// console.log(name);
+// var name = 'block-2';
+// console.log(name);
+// var name = 'block-3';
+// console.log(name);
+// var name = 'block-4';
+// console.log(name);
+// var name = 'block-5';
+// console.log(name);
+// var age = 31;
+// var name = 'ergash';
+// console.log(" mening ismim " + name + " mening yoshim " + age);
+
+// var num1 = 10;
+// var num2 = 5;
+
+// console.log(num1 - num2);
+// console.log(num1 + num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// var num1 = prompt(' 1raqam kiriting ');
+// var num2 = prompt(' 2raqam kiriting ');
+// alert(num1 * num2)
+
+// var name = prompt1('ismingiz');
+// console.log(" mening ismim " + name);
+// var age = prompt2(" yoshingiz ");
+// console.log(" yoshim " + age);
+
+var name = prompt('ismingiz');
+alert(" mening ismim " + name);
+var age = prompt(" yoshingiz ");
+alert(" yoshim " + age);
+
+var num1 = +prompt('6 + 3 = ?');
+console.log(" togri javob 6+3 = " + (6 + 3) + " sizning javobingiz " + num1);
